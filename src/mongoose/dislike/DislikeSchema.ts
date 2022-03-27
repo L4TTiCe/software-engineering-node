24bd26d0c91ded72e03da354aa6cf9c3e3a9352f
Dheshan Mohandass <dheshan.mail@gmail.com>
2022-03-26 22:35:43 -0400

    Create Dislike related mongoose Model and Schema
    Signed-off-by: Dheshan Mohandass <dheshan.mail@gmail.com>

---
+++ src/mongoose/dislike/DislikeSchema.ts
@@ -0,0 +1,17 @@
+/**
+ * @file Implements mongoose schema for Likes
+ */
+import mongoose from "mongoose"
+import {Dislike} from "../../models/Dislike"
+
+/**
+ * Represents dislikes relationship between a {@link User} and a {@link Tuit}, as in a user dislikes a tuit.
+ * The DislikeSchema represents how a Dislike is represented in the database.
+ * @typedef {DislikeSchema} DislikeSchema
+ * @property {Tuit} tuit - Tuit being disliked
+ * @property {User} likedBy - User disliking the tuit
+ */
+export const DislikeSchema = new mongoose.Schema<Dislike>({
+    tuit: {type: mongoose.Schema.Types.ObjectId, ref: "TuitModel"},
+    dislikedBy: {type: mongoose.Schema.Types.ObjectId, ref: "UserModel"},
+}, {collection: "dislikes"})
